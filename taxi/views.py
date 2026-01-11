f239d4d3fa10e8bced8d1c9d09d2310aa79eb155
AKupriichuk <alexa05kup@gmail.com>
2026-01-11 22:58:20 +0200

    c2

--- taxi/views.py
+++ taxi/views.py
@@ -1,4 +1,3 @@
-from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import generic
@@ -16,6 +15,7 @@ def index(request):
     num_manufacturers = Manufacturer.objects.count()
     num_visits = request.session.get("num_visits", 0)
     request.session["num_visits"] = num_visits +1
+
     context = {
         "num_drivers": num_drivers,
         "num_cars": num_cars,
